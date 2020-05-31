3374b9009aa1154716d364609cfd2667d0a71096
kishansinghchauhan <kishancoer7071@gmail.com>
2020-06-01 01:24:20 +0530

    sertup folder and file structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React,{Component} from 'react';
+import styles from './App.module.css';
+import Cards from './Components/Cards/Cards';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+import Chart from './Components/Chart/Chart';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
+
